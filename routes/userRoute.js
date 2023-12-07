4ec0472373b06cc6dc4ffc5624cf0bbd8f84b0bb
farouk <faaroukbnamor@gmail.com>
2023-12-07 07:44:59 +0100

    update issue fixing

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -29,7 +29,7 @@ app.post('/update', async (req, res) => {
     try {
         await User.findOneAndUpdate({_id: req.body._id} , req.body)
          const user = await User.findOne({_id: req.body._id}) 
-        res.send();
+        res.send(user);
     } catch (error) {
         res.status(400).json(error);
     }
